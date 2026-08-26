33384beeec62ec4acb9443954ed2cf51c088f0f1
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-08-26 19:08:53 +0200

    event fix

--- src/main/java/cz/maxtechnik/mteh/MtehMod.java
+++ src/main/java/cz/maxtechnik/mteh/MtehMod.java
@@ -8,6 +8,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
@@ -26,6 +27,7 @@ public class MtehMod{
 	public MtehMod(IEventBus bus){
 		bus.addListener(this::commonSetup);
 		bus.addListener(this::registerPayloads);
+		NeoForge.EVENT_BUS.register(this);
 	}
 	private void registerPayloads(RegisterPayloadHandlersEvent event){
 		PayloadRegistrar registrar=event.registrar("1").optional();
