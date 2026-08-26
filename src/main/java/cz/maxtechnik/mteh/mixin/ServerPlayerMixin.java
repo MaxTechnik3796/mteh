d62c2072b8e3bbb91f9aa4fd749575d9ae6bef14
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-08-26 17:39:02 +0200

    progress

--- src/main/java/cz/maxtechnik/mteh/mixin/ServerPlayerMixin.java
+++ src/main/java/cz/maxtechnik/mteh/mixin/ServerPlayerMixin.java
@@ -2,21 +2,19 @@ package cz.maxtechnik.mteh.mixin;
 
 import cz.maxtechnik.mteh.EnderHubMenu;
 import cz.maxtechnik.mteh.MtehModPackets;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundOpenScreenPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.MenuProvider;
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ChestMenu;
 import net.minecraft.world.inventory.PlayerEnderChestContainer;
 import net.neoforged.neoforge.network.PacketDistributor;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.OptionalInt;
@@ -25,55 +23,65 @@ import java.util.function.Consumer;
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin {
 
-	// 1. Zabalíme MenuProvider na začátku metody (volá se pouze 1x pro jakýkoliv kontejner)
-	@ModifyVariable(
-			method = "openMenu(Lnet/minecraft/world/MenuProvider;Ljava/util/function/Consumer;)Ljava/util/OptionalInt;",
-			at = @At("HEAD"),
-			argsOnly = true
-	)
-	private MenuProvider mteh$wrapMenuProvider(MenuProvider originalProvider) {
-		if (originalProvider == null) return null;
-		ServerPlayer player = (ServerPlayer) (Object) this;
+	@Shadow private int containerCounter;
+	@Shadow private void nextContainerCounter() {}
+	@Shadow private void initMenu(AbstractContainerMenu menu) {}
 
-		// Pokud hráč nemá náš mód, necháme původní provider beze změny
-		if (player.connection == null || !player.connection.hasChannel(MtehModPackets.OpenEnderChestPayload.TYPE)) {
-			return originalProvider;
-		}
-
-		return new MenuProvider() {
-			@Override
-			public @NotNull Component getDisplayName() {
-				return originalProvider.getDisplayName();
-			}
-
-			@Nullable
-			@Override
-			public AbstractContainerMenu createMenu(int containerId, @NotNull Inventory playerInventory, @NotNull Player player) {
-				AbstractContainerMenu menu = originalProvider.createMenu(containerId, playerInventory, player);
-
-				// Pokud je to Ender Chestka, nahradíme ji naším EnderHubMenu
-				if (menu instanceof ChestMenu chestMenu && chestMenu.getContainer() instanceof PlayerEnderChestContainer) {
-					return new EnderHubMenu(containerId, playerInventory, player.getEnderChestInventory());
-				}
-
-				// Pro barely a obyčejné truhly vrátíme původní menu nedotčené
-				return menu;
-			}
-		};
-	}
-
-	// 2. Po úspěšném otevření odešleme klientovi náš packet a data slotů
 	@Inject(
 			method = "openMenu(Lnet/minecraft/world/MenuProvider;Ljava/util/function/Consumer;)Ljava/util/OptionalInt;",
-			at = @At("RETURN")
+			at = @At("HEAD"),
+			cancellable = true
 	)
-	private void mteh$sendCustomPacketOnOpen(MenuProvider menuProvider, Consumer<AbstractContainerMenu> extraData, CallbackInfoReturnable<OptionalInt> cir) {
+	private void mteh$handleOpenMenu(MenuProvider menuProvider, Consumer<AbstractContainerMenu> extraData, CallbackInfoReturnable<OptionalInt> cir) {
+		if (menuProvider == null) {
+			cir.setReturnValue(OptionalInt.empty());
+			return;
+		}
+
 		ServerPlayer player = (ServerPlayer) (Object) this;
 
-		if (cir.getReturnValue().isPresent() && player.containerMenu instanceof EnderHubMenu customMenu) {
-			int containerId = cir.getReturnValue().getAsInt();
+		if (player.containerMenu != player.inventoryMenu) {
+			player.closeContainer();
+		}
+
+		this.nextContainerCounter();
+		AbstractContainerMenu menu = menuProvider.createMenu(this.containerCounter, player.getInventory(), player);
+		if (menu == null) {
+			if (player.isSpectator()) {
+				player.displayClientMessage(Component.translatable("container.spectatorCantOpen").withStyle(ChatFormatting.RED), true);
+			}
+			cir.setReturnValue(OptionalInt.empty());
+			return;
+		}
+
+		// 1. Ender Chest pro modovaného hráče
+		if (menu instanceof ChestMenu chestMenu && chestMenu.getContainer() instanceof PlayerEnderChestContainer
+				&& player.connection != null && player.connection.hasChannel(MtehModPackets.OpenEnderChestPayload.TYPE)) {
+
+			int containerId = this.containerCounter;
+			EnderHubMenu customMenu = new EnderHubMenu(containerId, player.getInventory(), player.getEnderChestInventory());
+			this.initMenu(customMenu);
+			player.containerMenu = customMenu;
+
+			// Odesíláme výhradně náš paket (žádný vanilla ClientboundOpenScreenPacket)
 			PacketDistributor.sendToPlayer(player, new MtehModPackets.OpenEnderChestPayload(containerId));
 			customMenu.sendAllDataToRemote();
+
+			cir.setReturnValue(OptionalInt.of(containerId));
+			return;
 		}
+
+		// 2. Všechny ostatní kontejnery (barely, normální truhly, pec, hráči bez módu)
+		if (extraData != null) {
+			extraData.accept(menu);
+		}
+
+		this.initMenu(menu);
+		if (player.connection != null) {
+			player.connection.send(new ClientboundOpenScreenPacket(menu.containerId, menu.getType(), menuProvider.getDisplayName()));
+		}
+		player.containerMenu = menu;
+
+		cir.setReturnValue(OptionalInt.of(this.containerCounter));
 	}
 }
